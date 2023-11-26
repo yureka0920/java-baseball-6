b68dc548ff4467574fd41665fa2aa3742452f0a0
방준호 <yureka0920@naver.com>
2023-11-27 06:19:46 +0900

    mvc 형태로 기초 구성.

---
+++ src/main/java/baseball/domain/BaseBall.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+public class BaseBall {
+
+    private final int strike;
+    private final int ball;
+
+    public BaseBall(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+
+
+}
